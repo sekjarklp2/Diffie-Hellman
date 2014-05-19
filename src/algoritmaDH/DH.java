841384db9f597437d3a4e4a8bf2d124d40779a4a
Danang Prawira N <danang.prawira.n@gmail.com>
2014-05-19 22:46:08 +0700

    sudah diperbaiki algo Diffie-Hellman + ane kasih contohnya

--- src/algoritmaDH/DH.java
+++ src/algoritmaDH/DH.java
@@ -3,65 +3,82 @@ package algoritmaDH;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DH {
-
+    /**
+    * Menghitung sebuah nDigit BigInteger prima 
+    *
+    * @param nDigit banyaknya digit dari bilangan yang akan dibuat
+    * @return            nDigit BigInteger Prima
+    */
     public static BigInteger getRandomPrime(int nDigit) {
         return BigInteger.probablePrime(nDigit, new SecureRandom());
     }
     
-    // Pre-condition: p is prime and 1 < a < p.
-    // Post-condition: returns true iff a is a primitive root of p.
-    public static boolean primitiveRoot(int a, BigInteger prime)
-    {
-        int p = prime.intValue();
-        
-        int val = a, i;
-        
-        // Calculate each modular exponent a^2, a^3... mod p.
-        for(i=2; i<p; i++) {
-            val = val*a%p;
-            if(val == 1)
-                break;
+    private static boolean testPrimitiveRoot(BigInteger a, BigInteger p)
+    {   
+        BigInteger i, val;
+        Set<BigInteger> hasil = new HashSet();
+        for(i=BigInteger.ONE; i.compareTo(p) < 0; i=i.add(BigInteger.ONE)) {
+            val = a.modPow(i, p);
+            if (hasil.contains(val)) return false;
+            else hasil.add(val);
         }
-        
-        // If the exponent is p-1, it's a primitive root
-        return (i == p-1);
+        return true;
     }
-    
-    public static BigInteger getPrimitiveRoot(BigInteger prime)
+    /**
+    * Mendapatkan sebuah primitive root dari prime
+    *
+    * @param p bilangan prima yang ingin dicari salah satu primitive rootnya
+    * @return            salah satu primitive root dari p
+    */
+    public static BigInteger getPrimitiveRoot(BigInteger p)
     {
-        int p = prime.intValue();
-        ArrayList<BigInteger> array = new ArrayList<BigInteger>();        
-        
-        for(int a=1; a<p; a++)
+        ArrayList<BigInteger> array = new ArrayList<>();   
+        for(BigInteger a=BigInteger.ONE;a.compareTo(p) < 0;a=a.add(BigInteger.ONE))
         {
-            if(primitiveRoot(a,prime))
-            {
-                BigInteger bigA = BigInteger.valueOf(a);
-                array.add(bigA);
-            }
+            if(testPrimitiveRoot(a,p))
+                array.add(a);
+            if (array.size() == 8) break;
         }
         
-        Collections.shuffle(array);
-        return (array.get(0));
-        
+        int idx = (int)(Math.random()*array.size());
+        return array.get(idx);
     }
-    
-    public static BigInteger getPrivateKey(BigInteger prime)
+    /**
+    * Menghitung secret key user tersebut
+    *
+    * @param p bilangan prima untuk menjadi patokan menghitung secret key
+    * @return            BigInteger secret key
+    */
+    public static BigInteger getSecretKey(BigInteger p)
     {
-        return prime.divide(BigInteger.valueOf((int)(Math.random()*100)));
+        return p.divide(BigInteger.valueOf((int)(Math.random()*100)));
     }
-    
-    public static BigInteger getPublicKet(BigInteger a, BigInteger Xa, BigInteger prime)
+    /**
+    * Menghitung nilai dari public key yang akan dikirimkan ke lawan komunikasi
+    *
+    * @param a BigInteger a primitive root dari p
+    * @param secretKey BigInteger secret key user tsb
+    * @param p BigInteger p
+    * @return            BigInteger hasil dari a^secretKey mod p
+    */
+    public static BigInteger getPublicKey(BigInteger a, BigInteger secretKey, BigInteger p)
     {
-        return a.modPow(Xa, prime);
+        return a.modPow(secretKey, p);
     }
-    
-    public static BigInteger getSessionKey(BigInteger Xa, BigInteger Xb, BigInteger a, BigInteger prime)
+    /**
+    * Menghitung nilai dari session key yang digunakan
+    *
+    * @param secretKey BigInteger private key user tsb
+    * @param publicKey BigInteger public key yang diterima dari lawan komunikasi
+    * @param p BigInteger p yang di generate user
+    * @return            BigInteger hasil dari publicKey^privateKey mod p
+    */
+    public static BigInteger getSessionKey(BigInteger secretKey, BigInteger publicKey, BigInteger p)
     {
-        return a.modPow((Xa.multiply(Xb)), prime);
-    }
-    
+        return publicKey.modPow(secretKey, p);
+    }   
 }
---
+++ src/algoritmaDH/DHExample.java
@@ -0,0 +1,29 @@
+package algoritmaDH;
+
+import java.math.BigInteger;
+
+public class DHExample {
+    public static void main(String[] args) {
+        // tentukan bilangan prima yang digunakan dan primitive rootnya
+        BigInteger p = DH.getRandomPrime(12);
+        BigInteger a = DH.getPrimitiveRoot(p);
+        
+        // generetan Alice
+        BigInteger Xa = DH.getSecretKey(p);
+        BigInteger Ya = DH.getPublicKey(a, Xa, p);
+        // Alice lalu ngirim public key ke Bob
+        
+        // generetan Bob
+        BigInteger Xb = DH.getSecretKey(p);
+        BigInteger Yb = DH.getPublicKey(a, Xb, p);
+        // Bob menghitung session key dengan public key Alice
+        BigInteger Kb = DH.getSessionKey(Xb, Ya, p);
+        // Bob lalu ngirim public key ke Alice
+        
+        // Alice menghitung session key
+        BigInteger Ka = DH.getSessionKey(Xa, Yb, p);
+        
+        // cetak apakah hasilnya sama
+        System.out.println("Ka: "+Ka+" Kb: "+Kb);
+    }
+}
